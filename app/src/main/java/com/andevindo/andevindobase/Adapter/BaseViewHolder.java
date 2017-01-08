3dce906775dc4e7fae963c3969cd7c94efab5476
Hendrawan <hendrawanadiwijaya@gmail.com>
2017-01-09 01:11:20 +0700

    Fix, base presenter on null

--- app/src/main/java/com/andevindo/andevindobase/Adapter/BaseViewHolder.java
+++ app/src/main/java/com/andevindo/andevindobase/Adapter/BaseViewHolder.java
@@ -13,24 +13,27 @@ public abstract class BaseViewHolder<T> extends RecyclerView.ViewHolder {
         setupView();
     }
 
-    public void setupView(){
+    public void setupView() {
 
     }
 
-    public void setBasePresenter(final T t, final BaseAdapter.BaseAdapterListener<T> presenter){
-        itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                presenter.onClick(t);
-            }
-        });
-        itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                presenter.onLongClick(t);
-                return true;
-            }
-        });
+    public void setBasePresenter(final T t, final BaseAdapter.BaseAdapterListener<T> presenter) {
+        if (presenter != null) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    presenter.onClick(t);
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    presenter.onLongClick(t);
+                    return true;
+                }
+            });
+        }
+
     }
 
     public abstract void bindData(final T t);
