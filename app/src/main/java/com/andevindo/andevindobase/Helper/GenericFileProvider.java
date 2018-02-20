06bb9b2957abf61f6559aa6610ad3f7469d05c9c
Hendrawan Adi Wijaya <hendrawanadiwijaya@gmail.com>
2018-02-20 07:30:01 +0700

    Add file provider to support api 24

---
+++ app/src/main/java/com/andevindo/andevindobase/Helper/GenericFileProvider.java
@@ -0,0 +1,13 @@
+package com.andevindo.andevindobase.Helper;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created on   : 2/20/2018
+ * Developed by : Hendrawan Adi Wijaya
+ * Github       : https://github.com/andevindo
+ * Website      : http://www.andevindo.com
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
